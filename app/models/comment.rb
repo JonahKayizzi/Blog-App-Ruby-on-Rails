0d914654a4cad3987d4598de6d69450b6d6e8e1a
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-01-19 19:28:59 +0300

    Add comment model and its methods

---
+++ app/models/comment.rb
@@ -0,0 +1,8 @@
+class Comment < ApplicationRecord
+    belongs_to :user, class_name: "User", foreign_key: "author_id"
+    belongs_to :post, class_name: "Post", foreign_key: "post_id"
+
+    def five_most_recent_comments_for_user(user)
+        user.comments.order(created_at: :desc).limit(5)
+    end
+end
