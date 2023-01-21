e42df8968a595e40a5e60872dcefaa628c15ece2
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-01-21 14:32:44 +0300

    Add model validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :likes, foreign_key: 'post_id'
   has_many :comments, foreign_key: 'post_id'
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :CommentsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :LikesCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   after_create :update_posts_counter_for_user
 
   def update_posts_counter_for_user(user)
