ca811e8bfe4d674c2f800128e8777f0e1c6bfe44
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-01-25 02:29:48 +0300

    Update controller methods to the required view logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,15 @@
 class UsersController < ApplicationController
+  layout 'application'
   def index
     @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
+    @top_three_posts = top_three_posts_for_user(@user)
+  end
+
+  def top_three_posts_for_user(user)
+    @posts = Post.where(author_id: user.id).order('created_at DESC').limit(3)
   end
 end
