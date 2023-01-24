a77b7da28ab67f6303c8ed266001451361162c74
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-01-25 02:31:04 +0300

    Update comments model foreign key constraint and db configurations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :user, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
 
   after_create :update_comments_counter_for_post
