f025a9f9018a732892625b92be9809e96957c793
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-01-27 20:32:42 +0300

    Add logic for submitting a new form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,28 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = Post.where(author_id: params[:user_id]).find(params[:id])
   end
+
+  def new
+    new_post = Post.new
+    respond_to do |format|
+      format.html { render :new, locals: { post: new_post } }
+    end
+  end
+
+  def create
+    post_params = params.require(:new_post).permit(:Title, :Text)
+    post = Post.new(post_params)
+    post.author = current_user
+    respond_to do |format|
+      format.html do
+        if post.save
+          flash[:notice] = "Post created successfully"
+          redirect_to users_path
+        else
+          flash.now[:alert] = "Post creation failed"
+          render :new, locals: { post: post }
+        end
+      end
+    end
+  end
 end
