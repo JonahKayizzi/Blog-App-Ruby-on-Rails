5f658fb7dbc282031c53c2d9f461823bd2b54254
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-02-02 19:53:45 +0300

    Fix linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
-    before_action :authenticate_user!
-  
-    private
-  
-    def after_sign_out_path_for(resource_or_scope)
-      new_user_session_path
-    end
-  
-    protected
-  
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: %i[email name bio password password_confirmation])
-    end
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
+  private
+
+  def after_sign_out_path_for(*)
+    new_user_session_path
   end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email name bio password password_confirmation])
+  end
+end
